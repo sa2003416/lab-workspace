a23ea691ed02888193bdc096aa2a7af3790ff105
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-09-03 15:41:21 +0300

    update

---
+++ B52-Wednesday/Lab 2 Dart Fundamentals/functions.dart
@@ -0,0 +1,12 @@
+void main() {
+  print(add(11, 22));
+  print(add(44, 33));
+}
+
+int add(int a, int b) {
+  return a + b;
+}
+
+int add2(a, b) {
+  return a + b;
+}
---
+++ B52-Wednesday/Lab 2 Dart Fundamentals/loops.dart
@@ -0,0 +1,17 @@
+void main() {
+  // for loop
+  for (int i = 0; i < 5; i++) {
+    print(i);
+  }
+
+  int x = 0;
+  while (x < 5) {
+    print(x);
+    x++;
+  }
+
+  do {
+    print(x);
+    x++;
+  } while (x < 5);
+}
