ad38b92d68dcbfdb2b8884f46a47a406de552aca
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-09-07 17:44:16 +0300

    book entity

---
+++ B03-Sunday/Lab 3-Dart OOP and Lambdas/library_app/lib/domain/entities/book.dart
@@ -0,0 +1,34 @@
+class Book {
+  String name;
+  String author;
+  int yearOfPublication;
+
+  // Book(String name, String author, int yearOfPublication) {
+  //   this.name = name;
+  //   this.author = author;
+  //   this.yearOfPublication = yearOfPublication;
+  // }
+  Book({
+    required this.name,
+    required this.author,
+    required this.yearOfPublication,
+  });
+
+  // have  a way to change this object into json
+
+  Map<String, dynamic> toJSON() {
+    return {
+      "name": name,
+      "author": author,
+      "yearOfPublication": yearOfPublication,
+    };
+  }
+
+  factory Book.fromJSON(Map<String, dynamic> json) {
+    return Book(
+      name: json["name"],
+      author: json["author"],
+      yearOfPublication: json["yearOfPublication"],
+    );
+  }
+}
