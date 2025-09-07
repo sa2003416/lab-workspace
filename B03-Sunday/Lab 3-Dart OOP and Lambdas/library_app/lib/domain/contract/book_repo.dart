ff45d1b74775ae1a356f3405dbdcbf3bfd884232
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-09-07 18:32:08 +0300

    mixin

---
+++ B03-Sunday/Lab 3-Dart OOP and Lambdas/library_app/lib/domain/contract/book_repo.dart
@@ -0,0 +1,8 @@
+import 'package:library_app/domain/entities/book.dart';
+
+abstract class BookRepo {
+  List<Book> getBooks();
+  void addBook(Book book);
+  void updateBook(Book book);
+  void deleteBook(String name);
+}
