bc2a916f3e393855ed5552b6a69530e6221279b8
Abdulahi Hassen <abdala.baaji@gmail.com>
2025-09-07 16:26:43 +0300

    repos

--- B51-Sunday/Lab 3-Dart OOP and Lambdas/my_library/bin/my_library.dart
+++ B51-Sunday/Lab 3-Dart OOP and Lambdas/my_library/bin/my_library.dart
@@ -1,5 +1,14 @@
+import 'dart:math';
+
 import 'package:my_library/my_library.dart' as my_library;
 
 void main(List<String> arguments) {
   print('Hello world: ${my_library.calculate()}!');
+
+  List<int> numbers = [1, 2, 3, 4, 5, 6];
+
+  var mapped = numbers.map((n) => "-$n");
+  var mapped2 = numbers.map((n) => pow(n, 2));
+  print(mapped);
+  print(mapped2);
 }
--- B51-Sunday/Lab 3-Dart OOP and Lambdas/my_library/lib/data/repositories/json_book_repo.dart
+++ B51-Sunday/Lab 3-Dart OOP and Lambdas/my_library/lib/data/repositories/json_book_repo.dart
@@ -1,10 +1,30 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:my_library/domain/contracts/book_repo.dart';
 import 'package:my_library/domain/entities/book.dart';
 
 class JsonBookRepo implements BookRepo {
   List<Book> books = [];
 
+  JsonBookRepo() {
+    readBooksFromJSON();
+  }
+
   // method that will read from the json
+
+  // read the books from the json files
+  void readBooksFromJSON() {
+    String fileName = 'assets/data/books.json';
+    File file = File(fileName);
+
+    String content = file.readAsStringSync();
+
+    // convert the list
+    List<dynamic> jsonBooks = jsonDecode(content);
+
+    books = jsonBooks.map((json) => Book.fromJson(json)).toList();
+  }
 
   @override
   void addBook(Book book) {
